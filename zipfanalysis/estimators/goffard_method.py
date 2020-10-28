5cee1ec969e99ff90cbc6e307a56135a5db8984f
chasmani <pilgrimcharlie2@gmail.com>
2020-10-28 17:58:16 +0000

    Goffard working with exponential

--- zipfanalysis/estimators/goffard_method.py
+++ zipfanalysis/estimators/goffard_method.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from statsmodels.nonparametric.kde import KDEUnivariate
 import scipy.stats
+import scipy.special
 
 def get_normal_p(x_i, variance, mean):
 	p_i = 1/(math.sqrt(2*math.pi*variance)) * np.exp(-0.5 * ((x_i - mean)/math.sqrt(variance))**2)
@@ -112,6 +113,20 @@ def get_weights(a_ks, d_ks, prior_mean, prior_variance, kde, epsilon):
 			ws.append(w_k)
 	return np.array(ws)
 
+def get_weights_gamma_prior(a_ks, d_ks, prior_alpha, prior_beta, kde, epsilon):
+
+	ws = []
+	for k in range(len(a_ks)):
+		if d_ks[k] > epsilon:
+			ws.append(0)
+		else:
+			a_k = a_ks[k]
+			prior_p = get_gamma_p(a_k, prior_alpha, prior_beta)
+			kde_p = kde.evaluate(a_k)[0]
+			w_k = prior_p/kde_p
+			ws.append(w_k)
+	return np.array(ws)	
+
 
 
 
@@ -129,5 +144,87 @@ def test_goffard():
 	plt.show()
 	
 
+
+def goffard_exponential(x, prior_alpha, prior_beta):
+
+	n_particles = 1024
+	n_data = len(x)
+	alpha=0.2
+
+	# 1. Draw particles from prior
+	a_k = np.random.gamma(shape=prior_alpha, scale=1/prior_beta, size=n_particles)
+
+	#2. KDE of particles
+	weights = np.array([0.1]*len(a_k))
+	kde = scipy.stats.gaussian_kde(a_k, weights=weights)
+	
+	epsilon = 10000
+
+	for g in range(5):
+		print("Generation ", g)
+		d_ks = []
+		a_ks = []
+		for k in range(n_particles):
+			hit = False
+			while not hit:
+				a_k = kde.resample(size=1)[0][0]
+				# a_k below zero can be ignored I think- they won't hit
+				if a_k > 0:
+					z_k = np.random.exponential(scale=1/a_k, size=n_data)
+					d_k = scipy.stats.wasserstein_distance(z_k, x)
+					if d_k <= epsilon:
+						d_ks.append(d_k)
+						a_ks.append(a_k)
+						hit=True
+
+		# set new epsilon
+		successful_as,successful_ds, epsilon = extract_successful_trials(a_ks, d_ks, alpha)
+
+		print("Epsilon is now ", epsilon)
+
+		ws = get_weights_gamma_prior(a_ks, d_ks, prior_alpha, prior_beta, kde, epsilon)
+		print(ws)
+
+		kde = scipy.stats.gaussian_kde(a_ks, weights=ws)
+
+
+
+
+	xs = np.linspace(0,1)
+	kdes = [kde.evaluate(x_i) for x_i in xs]
+	plt.plot(xs, kdes, label="WABC")
+
+
+
+
+
+
+def plot_likelihood_exponential(x, prior_alpha, prior_beta):
+
+	posterior_alpha = prior_alpha + len(x)
+	posterior_beta = prior_beta + sum(x)
+
+	xs = np.linspace(0,2)
+	ps = [get_gamma_p(x, posterior_alpha, posterior_beta) for x in xs]
+	plt.plot(xs, ps, label="Actual Posterior")
+
+def get_gamma_p(x, alpha, beta):
+	p = (beta**alpha) /scipy.special.gamma(alpha) * x**(alpha-1) * np.exp(-1*beta*x)
+	return p
+
+def test_goffard_exponential():
+
+	np.random.seed(1)
+	# Gamma is a conjugate prior
+	prior_alpha = 1
+	prior_beta = 1
+
+	actual_lamb = 0.6
+	x = np.random.exponential(scale=1/actual_lamb, size=100)
+	plot_likelihood_exponential(x, prior_alpha, prior_beta)
+	goffard_exponential(x, prior_alpha, prior_beta)
+	plt.show()
+
+
 if __name__=="__main__":
-	test_goffard()
+	test_goffard_exponential()
