b2f8073186b4f1a14b9462ff3a8f4ef9970ce0f8
chasmani <pilgrimcharlie2@gmail.com>
2020-11-03 14:13:52 +0000

    Ready to run beaumont on Wawirkc cluster

--- zipfanalysis/estimators/plot_bias.py
+++ zipfanalysis/estimators/plot_bias.py
@@ -3,19 +3,26 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-input_filename = "goffard_overnight_results_for_figure.csv"
+def plot_goffard():
 
-names = ["seed", "alpha", "method", "bw", "succ_prop", "gens", "n_particles", "alpha_hat", "time"]
+	input_filename = "goffard_overnight_results_for_figure.csv"
+	names = ["seed", "alpha", "method", "bw", "succ_prop", "gens", "n_particles", "alpha_hat", "time"]
+	df = pd.read_csv(input_filename, names=names, sep=";")
+	df = df[(df["method"] == "goffard") & (df["succ_prop"] == 0.1) & (df["gens"] == 4)]
+	df["bias"] = df["alpha_hat"] - df["alpha"]
+	df_means = df.groupby("alpha").mean().reset_index()
+	sns.scatterplot(x="alpha", y="bias", data=df_means)
+	plt.show()
 
-df = pd.read_csv(input_filename, names=names, sep=";")
+def plot_beaumont():
+	input_filename = "zipf_beaumont_results.csv"
+	names = ["method", "seed", "exponent", "n_particles", "survival_fraction", 
+					"n_data", "generations", "mle", "time"]
+	df = pd.read_csv(input_filename, names=names, sep=";")
+	df = df[(df["method"] == "Beaumont 2007 Basic")]
+	df["bias"] = df["mle"] - df["exponent"]
+	df_means = df.groupby("exponent").mean().reset_index()
+	sns.scatterplot(x="exponent", y="bias", data=df)
+	plt.show()	
 
-
-df = df[(df["method"] == "goffard") & (df["succ_prop"] == 0.1) & (df["gens"] == 4)]
-
-df["bias"] = df["alpha_hat"] - df["alpha"]
-
-df_means = df.groupby("alpha").mean().reset_index()
-
-
-sns.scatterplot(x="alpha", y="bias", data=df_means)
-plt.show()
+plot_beaumont()
